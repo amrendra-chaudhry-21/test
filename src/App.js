c85d56f86d3e1aee07d12ed8e77bbcd83297ee4b
amrendrait43@gmail.com <Nh2conh2@aa432>
2022-07-21 14:26:59 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Workingcomponent from "./Components/todoListCompo/Workingcomponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="containerr">
+        <div className="component">
+          <div className="heading">
+            <h1 className="heading">RenewBuy Project</h1>
+          </div>
+          <Workingcomponent />
+        </div>
+      </div>
+    </>
   );
 }
 
